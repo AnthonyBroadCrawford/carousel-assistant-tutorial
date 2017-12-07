ce8a85b2a72e57ad49d8a31e7d47ce55db62d9a3
Anthony Broad-Crawford <abc@abc.local>
2017-12-06 19:11:37 -0500

    missed binding the router to the main app

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+process.env.DEBUG = 'actions-on-google:*';
+
+const App = require('actions-on-google').DialogflowApp;
+const firebase = require('firebase-functions');
+
+exports.carouselAssistant = firebase.https.onRequest((request, response) => {
+
+  const app = new App({request, response});
+
+  //I'm logging this into my firebase console so I can learn the data-structures Google is passing to the app.
+  console.log('Request headers: ' + JSON.stringify(request.headers));
+  console.log('Request body: ' + JSON.stringify(request.body));
+
+  function displayCarousel(app){
+    app.tell('Hello!');
+  }
+
+  let router = new Map();
+  router.set("display_carousel", displayCarousel);
+  app.handleRequest(router);
+});
