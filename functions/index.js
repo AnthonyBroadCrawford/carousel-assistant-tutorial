9420b8dd576eebbea3a951345844d8dc07142efa
Anthony Broad-Crawford <abc@abc.local>
2017-12-06 21:43:00 -0600

    have first rich response working

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,20 @@ exports.carouselAssistant = firebase.https.onRequest((request, response) => {
   console.log('Request body: ' + JSON.stringify(request.body));
 
   function displayCarousel(app){
-    app.tell('Hello!');
+    app.ask(app.buildRichResponse()
+      // Create a basic card and add it to the rich response
+      .addSimpleResponse('Math and prime numbers it is!')
+      .addBasicCard(app.buildBasicCard('42 is an even composite number. It' +
+        'is composed of three distinct prime numbers multiplied together. It' +
+        'has a total of eight divisors. 42 is an abundant number, because the' +
+        'sum of its proper divisors 54 is greater than itself. To count from' +
+        '1 to 42 would take you about twenty-one…')
+        .setTitle('Math & prime numbers')
+        .addButton('Read more', 'https://example.google.com/mathandprimes')
+        .setImage('https://example.google.com/42.png', 'Image alternate text')
+        .setImageDisplay('CROPPED')
+      )
+    );
   }
 
   let router = new Map();
